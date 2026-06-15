683b6fbbca9668c5ecdc5a554a85ce6c77c65cea
masaya.ozaki <ozamasa0926@gmail.com>
2026-06-15 17:14:40 +0900

    changes for amplify

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>Hello from Amplify</h1>
+      </header>
+    </div>
+  );
+}
+export default App;
